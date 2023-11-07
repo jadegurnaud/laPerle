2fc28e31a3a5f956dfa3d5b3eda4b7a7136e5dca
Arthur DUBOIS <arthur.dbs70@gmail.com>
2023-11-07 16:21:10 +0100

    create commande files

---
+++ src/Form/CommandeType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Commande;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class CommandeType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('total')
+            ->add('livraison_id')
+            ->add('client_id')
+            ->add('paiement')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Commande::class,
+        ]);
+    }
+}
