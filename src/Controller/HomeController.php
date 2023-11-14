2e2c4f9a7bcba95fc3153fe322441655f8b72977
jadegurnaud <jade.gurnaud@gmail.com>
2023-11-14 13:50:45 +0100

    commandes accueil

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -9,16 +9,19 @@ use Symfony\Component\HttpFoundation\Request;
 use Doctrine\ORM\EntityManagerInterface;
 use App\Entity\Client;
 use App\Repository\ClientRepository;
+use App\Repository\CommandeRepository;
 use App\Form\ClientType;
 
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'accueil')]
-    public function index(ClientRepository $clientRepository): Response
+    public function index(ClientRepository $clientRepository, CommandeRepository $commandeRepository): Response
     {
         $clients = $clientRepository->findAll();
+        $commandes = $commandeRepository->findAll();
         return $this->render('home/index.html.twig', [
-            'clients' => 'clients',
+            'clients' => $clients,
+            'commandes' => $commandes,
         ]);
     }
 }
